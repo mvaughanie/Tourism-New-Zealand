883a281a3a0373f91cfbe9b8281e117333f6f881
Matthew <mvaughanie@gmail.com>
2020-07-03 11:03:40 +1200

    Fuel and Cost calculated, Shows confirmation screen

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,65 +1,96 @@
 $(function () {
 
-var vehicleArray = [];
-const el_welcome = $('#welcome');
-const el_vehicle = $('#filtered');
-var event = new Object();
+    var vehicleArray = [];
+    const el_body = $('body');
+    const el_welcome = $('#welcome');
+    const el_vehicle = $('#filtered');
+    const el_confirm = $('#confirm');
+    var fuelPrice = 2;
+    var event = new Object();
 
-function init() {
-    $.getJSON('json/vehicles.json', function (data) {
-        vehicleArray = data.vehicles;
-        displayVehicles(vehicleArray);
-    });
+    function init() {
+        $.getJSON('json/vehicles.json', function (data) {
+            vehicleArray = data.vehicles;
+            displayVehicles(vehicleArray);
+        });
 
-    addSubmitListener();
-    el_vehicle.hide();
-}
-
-//---EVENT LISTENERS----------------------------------------------------------
-
-//---COLLECT INPUT DATA-------------------------------------------------------
-
-function addSubmitListener() {
-    $('#submit_button').click(function () {
-        event.name = $('#name').val();
-        event.email = $('#email').val();
-        event.people = $('#people').val();
-        event.days = $('#days').val();
-        event.estimatedDistance = $('#estimated-distance').val();
-
-        displayVehicles(vehicleArray);
-        addAcceptListener();
-        el_welcome.hide();
-        el_vehicle.show();
-        
-    });
-};
-
-function addAcceptListener() {
-    $('#accept_button').click(function () {
-        let totalFuel = this.estimatedDistance * this.fuelConsumption;
-        event.totalFuel = totalFuel;
-        console.log(this.estimatedDistance);
+        addSubmitListener();
         el_vehicle.hide();
-    })
-}
+        el_confirm.hide();
+    };
+
+    //---EVENT LISTENERS----------------------------------------------------------
+
+    //---COLLECT INPUT DATA-------------------------------------------------------
+
+    function addSubmitListener() {
+        $('#submit_button').click(function () {
+            event.name = $('#name').val();
+            event.email = $('#email').val();
+            event.people = $('#people').val();
+            event.days = $('#days').val();
+            event.estimatedDistance = $('#estimated-distance').val();
+
+            displayVehicles(vehicleArray);
+
+            $(document).scrollTop(0)
+
+            el_welcome.hide();
+            el_vehicle.show();
+
+        });
+    };
+
+    function addAcceptListener() {
+        $('.vehicle__submit').click(function () {
+            event.title = $(this).data('title');
+            event.cost = $(this).data('daily-cost');
+            event.fuel = $(this).data('fuel');
+            event.totalFuel = event.fuel * event.estimatedDistance;
+            event.totalCost = event.cost * event.days;
+            
+            
+            console.log(event);
+
+            let html = '';
+            html += makeConfirmationHTML()
+            el_confirm.html(html);
+            addCompleteListener();
+
+            // el_vehicle.hide();
+            $('body,html').css('overflow','hidden');
+            $(document).scrollTop(0)
+            el_confirm.show();
+            
+        });
+    };
+
+    function addCompleteListener(){
+        $('#complete-button').click(function() {
+            el_confirm.hide();
+            el_vehicle.hide();
+            el_welcome.show();
+            $('body,html').css('overflow','scroll');
+            $(document).scrollTop(0)
+        });
+    };
 
 
-//---DISPLAY OUTPUT DATA------------------------------------------------------
+    //---DISPLAY OUTPUT DATA------------------------------------------------------
 
-function displayVehicles(vehicle) {
-    let html = '';
+    function displayVehicles(vehicle) {
+        let html = '';
 
-    for (var i =0; i < vehicle.length; i++){
-        html += makeVehicleHTML(vehicle[i]);
-    }
-    el_vehicle.html(html);
-    // addAcceptListener();
-}
+        for (var i = 0; i < vehicle.length; i++) {
+            html += makeVehicleHTML(vehicle[i]);
+        }
+        el_vehicle.html(html);
+        addAcceptListener();
+    };
 
-function makeVehicleHTML(vehicle){
-    return `
-    <div class="vehicle">
+    function makeVehicleHTML(vehicle) {
+        return `
+        <div class="vehicle">
             <div class="vehicle__thumbnail">
                 <!-- <img src="images/small.jpg" alt=""> -->
 
@@ -76,28 +107,43 @@ function makeVehicleHTML(vehicle){
                     </div>
                     
                     <div class="vehicle__figures">
-                        <p id="cost">${vehicle.dailyCost}</p>
+                        <p id="cost">$${vehicle.dailyCost}</p>
                         <p id="total-cost">$${vehicle.dailyCost * event.days}</p>
-                        <p id="fuel">${vehicle.fuelConsumption}</p>
-                        <p id="total-fuel">$${event.estimatedDistance}</p>
+                        <p id="fuel">${vehicle.fuelConsumption}L</p>
+                        <p id="total-fuel">$${(((event.estimatedDistance / 100) * vehicle.fuelConsumption) * fuelPrice).toFixed(2)}</p>
                     </div>
                 </div>
                 
                 <div class="submit-div">
-                    <input class="vehicle__submit" id="accept_button" type="button" value="Accept">
+                    <input class="vehicle__submit" id="accept_button" type="button" value="Accept" data-daily-cost="${vehicle.dailyCost}" data-fuel="${vehicle.fuelConsumption}" data-title="${vehicle.title}">
                 </div>
                 
             </div>
         </div>
         
     `
-}
+    };
+
+    function makeConfirmationHTML() {
+
+        return `
+        <div class="confirm__box">
+            <h3 class="confirm__head" >Booking Confirmed</h3>
+            <p class="confirm__para" >Thank you ${event.name} for booking a ${event.title}</p>
+            <p class="confirm__para" >More information has been sent to your email</p>
+            <p class="confirm__para" >Enjoy New Zealand</p>
+
+            <div class="submit-div">
+                <input class="vehicle__submit" id="complete-button" type="button" value="Accept">
+            </div>
+        </div>
+        `
+    };
 
 
 
 
 
-
-init();
+    init();
 
 });
